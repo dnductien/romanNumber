4ff51078ea836a774f979adba1e45cdb12f194f2
ductien.dangnguyen <ductien.dangnguyen@gmail.com>
2021-08-30 16:03:28 +0200

    void convertNumberThreeToIII() failed

--- src/test/java/RomanNumberTest.java
+++ src/test/java/RomanNumberTest.java
@@ -13,7 +13,15 @@ public class RomanNumberTest {
         assertEquals("II", toRoman(2));
     }
 
+    @Test
+    void convertNumberThreeToIII() {
+        assertEquals("III", toRoman(3));
+    }
+
     private String toRoman(int i) {
+        if (i == 2) {
+            return "II";
+        }
         return "I";
     }
 }
