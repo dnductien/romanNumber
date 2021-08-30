1ad00bf7e4052c55a7ae3d9ea48589d597477a1a
ductien.dangnguyen <ductien.dangnguyen@gmail.com>
2021-08-30 15:41:20 +0200

    Initial commit

---
+++ src/test/java/RomanNumberTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumberTest {
+    @Test
+    void convertNumberOneToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return "";
+    }
+}
